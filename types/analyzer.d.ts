c9b02e23af3c237b9f072d61f3609390b30654ee
dhowe <dhowe@cims.nyu.edu>
2023-12-14 12:46:42 +0800

    nupr

--- types/analyzer.d.ts
+++ types/analyzer.d.ts
@@ -3,7 +3,7 @@ declare class Analyzer {
     constructor(parent: any);
     cache: {};
     RiTa: any;
-    lts: LetterToSound;
+    lts: any;
     analyze(text: any, opts: any): {
         phones: string;
         stresses: string;
@@ -11,11 +11,10 @@ declare class Analyzer {
         pos: any;
         tokens: any;
     };
-    computePhones(word: any, opts: any): any[];
+    computePhones(word: any, opts: any): any;
     phonesToStress(phones: any): string;
     analyzeWord(word: any, opts?: {}): any;
     _computeRawPhones(word: any, lex: any, opts: any): any;
     _computePhonesHyph(word: any, lex: any, opts: any): any[];
     _computePhonesWord(word: any, lex: any, opts: any, isPart: any): any;
 }
-import LetterToSound from "./rita_lts.js";
